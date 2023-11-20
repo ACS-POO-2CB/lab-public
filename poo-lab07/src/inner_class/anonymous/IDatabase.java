5c9992b6a4272c969673064d3f979f783d6df59c
Alexandru Dima <alexdima98@gmail.com>
2023-11-20 11:29:18 +0200

    Update IDatabase.java

--- poo-lab07/src/inner_class/anonymous/IDatabase.java
+++ poo-lab07/src/inner_class/anonymous/IDatabase.java
@@ -1,16 +1,18 @@
 /**
- * UPB, ACS, POO CB 2022-2023
- * @author Gabriel Gutu-Robu
- *
  * DO NOT MODIFY THIS FILE
  */
 package inner_class.anonymous;
+
+import java.util.ArrayList;
 
 public interface IDatabase {
     void connect();
     void disconnect();
 
     void insert(Object object);
-    void update(Object object);
+    void update(Object object, Object newObject);
     void delete(Object object);
+
+    ArrayList<Object> getAll();
 }
+
